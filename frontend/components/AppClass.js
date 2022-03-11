6aca29fd2e3927ff7a6d0ed31dae0e53a19e29c5
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-10 18:53:23 -0800

    got the steps to show up on the screen

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -94,7 +94,7 @@ export default class AppClass extends React.Component {
       <div id="wrapper" className={className}>
         <div className="info">
           <h3 id="coordinates">Coordinates  ({this.state.x}, {this.state.y} ) </h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="steps">You moved {this.state.steps} times</h3>
         </div>
         <div id="grid">
           <div className="square"></div>
