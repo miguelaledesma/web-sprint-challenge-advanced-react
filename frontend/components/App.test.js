4453bedeadd04ad561745abdaad6ff2280c8bec3
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-15 21:25:56 -0700

    this sprint took a long time, finally finished all tests and test.js is completed

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,11 @@
+import AppFunctional from "./AppFunctional";
+import { render, screen, waitFor } from '@testing-library/react';
+import React from 'react'
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+// test('sanity', () => {
+//   expect(true).toBe(false)
+// })
+
+test('renders without errors', () => {
+  render(<AppFunctional /> );
+});
